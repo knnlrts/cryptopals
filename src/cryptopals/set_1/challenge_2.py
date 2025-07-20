99297ca11e2888263278793f328108698c2aef70
knnlrts <koen.naulaerts@gmail.com>
2025-07-20 16:47:18 +0200

    cleanup

--- src/cryptopals/set_1/challenge_2.py
+++ src/cryptopals/set_1/challenge_2.py
@@ -4,14 +4,18 @@
 # dependencies = []
 # ///
 
-# challenge 2: fixed XOR
+# Challenge 2: fixed XOR
+#
 # Write a function that takes two equal-length buffers and produces their XOR combination.
+#
 # If your function works properly, then when you feed it the string:
-# 1c0111001f010100061a024b53535009181c
+#   1c0111001f010100061a024b53535009181c
+#
 # ... after hex decoding, and when XOR'd against:
-# 686974207468652062756c6c277320657965
+#   686974207468652062756c6c277320657965
+#
 # ... should produce:
-# 746865206b696420646f6e277420706c6179
+#   746865206b696420646f6e277420706c6179
 
 
 def fixed_xor(hex_1: str, hex_2: str) -> str:
--- src/cryptopals/set_1/challenge_4.py
+++ src/cryptopals/set_1/challenge_4.py
@@ -4,7 +4,8 @@
 # dependencies = []
 # ///
 
-# challenge 4: Detect single-character XOR
+# Challenge 4: Detect single-character XOR
+#
 # One of the 60-character strings in this file has been encrypted by single-character XOR.
 # Find it.
 # (Your code from #3 should help.)
