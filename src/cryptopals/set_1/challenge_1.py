a1c6d0cd84eb4899b4f17eb2257eabc871dce5b0
knnlrts <koen.naulaerts@gmail.com>
2025-07-20 12:28:43 +0200

    refactor challenge 1 and 2

--- src/cryptopals/set_1/challenge_1.py
+++ src/cryptopals/set_1/challenge_1.py
@@ -4,25 +4,44 @@
 # dependencies = []
 # ///
 
-# challenge 1: convert hex to base64
+# challenge 1: Convert hex to base64
+# The string:
+# 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# Should produce:
+# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+# So go ahead and make that happen. You'll need to use this code for the rest of the exercises.
+# Cryptopals Rule:
+# Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+
 
 import base64
 
-hex_string = "49276D206B696C6C696E6720796F757220627261696E206C696B65206120706F69736F6E6F7573206D757368726F6F6D"
-EXPECTED_BASE64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+test_hex_string = "49276D206B696C6C696E6720796F757220627261696E206C696B65206120706F69736F6E6F7573206D757368726F6F6D"
+test_base64_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-raw_bytes = bytes.fromhex(hex_string)
-print(raw_bytes)
-print(type(raw_bytes))
-print(f"{int(hex_string, 16):b}")
-print(f"{int(hex_string, 16):o}")
-print(f"{int(hex_string, 16):x}")
+# print(f"{int(hex_string, 16):b}")
+# print(f"{int(hex_string, 16):o}")
+# print(f"{int(hex_string, 16):x}")
 
-assert f"{int(hex_string, 16):x}".upper() == hex_string
 
-encoded_base64 = base64.b64encode(raw_bytes).decode("ascii")
-print(encoded_base64)
-print(type(encoded_base64))
+def hex_to_base64(hex_str: str) -> str:
+    assert len(hex_str) % 2 == 0
+    raw_bytes = bytes.fromhex(hex_str)
+    print(raw_bytes)
+    print([format(byte, "08b") for byte in raw_bytes])
+    encoded_base64 = base64.b64encode(raw_bytes).decode()
+    print(encoded_base64)
+    return encoded_base64
 
-assert encoded_base64 == EXPECTED_BASE64
-print("OK!")
+
+def base64_to_hex(base64_str: str) -> str:
+    raw_bytes = base64.b64decode(base64_str)
+    print(raw_bytes)
+    print([bin(byte)[2:].zfill(8) for byte in raw_bytes])
+    hex_str = raw_bytes.hex()
+    print(hex_str)
+    return hex_str
+
+
+assert hex_to_base64(test_hex_string) == test_base64_string
+assert base64_to_hex(test_base64_string) == test_hex_string.lower()
--- src/cryptopals/set_1/challenge_2.py
+++ src/cryptopals/set_1/challenge_2.py
@@ -13,20 +13,33 @@
 # ... should produce:
 # 746865206b696420646f6e277420706c6179
 
+
 hex_str1 = "1c0111001f010100061a024b53535009181c"
 hex_str2 = "686974207468652062756c6c277320657965"
-
-
-def hex_xor(hex_1: str, hex_2: str) -> str:
-    assert len(hex_1) == len(hex_2)
-    raw_1 = int(hex_1, 16)
-    raw_2 = int(hex_2, 16)
-    xor = raw_1 ^ raw_2
-    return f"{xor:x}"
-
-
 hex_out = "746865206b696420646f6e277420706c6179"
-print(hex_xor(hex_str1, hex_str2))
 
-assert hex_xor(hex_str1, hex_str2) == hex_out
-print("OK!")
+
+def fixed_xor(hex_1: str, hex_2: str) -> str:
+    assert len(hex_1) == len(hex_2)
+
+    raw_bytes_1 = bytes.fromhex(hex_1)
+    print(hex_1)
+    print(raw_bytes_1)
+    print([format(byte, "08b") for byte in raw_bytes_1])
+
+    raw_bytes_2 = bytes.fromhex(hex_2)
+    print(hex_2)
+    print(raw_bytes_2)
+    print([format(byte, "08b") for byte in raw_bytes_2])
+
+    xored_bytes = bytes([a ^ b for (a, b) in zip(raw_bytes_1, raw_bytes_2)])
+    print(xored_bytes)
+    print([format(byte, "08b") for byte in xored_bytes])
+
+    xored_hex_str = xored_bytes.hex()
+    print(xored_hex_str)
+
+    return xored_hex_str
+
+
+assert fixed_xor(hex_str1, hex_str2) == hex_out
